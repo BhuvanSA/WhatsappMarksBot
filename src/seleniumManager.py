2defb1f498442eccb6b7e9c8ae00be22be30911c
Bhuvan S A <bhuvansaraj2003@gmail.com>
2024-01-23 20:10:47 +0530

    revert changes

--- src/seleniumManager.py
+++ src/seleniumManager.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.options import Options
 import time
 
 
@@ -19,9 +18,7 @@ class SeleniumManager:
 
     def __init__(self):
         """Initializes the SeleniumManager with a new Chrome browser instance."""
-        chrome_options = Options()
-        chrome_options.add_argument("--headless")
-        self.browser = webdriver.Chrome(options=chrome_options)
+        self.browser = webdriver.Chrome()
 
     def open_whatsapp(self):
         """Opens WhatsApp Web and waits for the user to log in."""
